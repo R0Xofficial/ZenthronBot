3ff9ff65b357766781cc22196461f23ab8a0d46a
R0X <patryk.bieganski1003@gmail.com>
2025-06-20 22:07:29 +0200

    Update zenthron.py

--- zenthron.py
+++ zenthron.py
@@ -6,11 +6,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# --- ZenthronBot [Development] ---
-# This is the development environment where the bot code is created.
-# This code is in active development.
-# It may contain bugs, unfinished features, or not work at all.
-# Use for testing and development only!
+# --- ZenthronBot ---
 
 import logging
 import random
