82521f57a1ec133336da954fb5a9b56b725020af
R0X <patryk.bieganski1003@gmail.com>
2025-06-20 22:08:47 +0200

    Create envtemp.sh

---
+++ envtemp.sh
@@ -0,0 +1,31 @@
+# ZenthronBot - Enviroment file
+# Copyright (C) 2025 R0X
+# Licensed under the GNU General Public License v3.0
+# See the LICENSE file for details.
+
+#!/bin/bash
+
+# Set your BOT token here in "".
+# To generate a BOT token, create your bot with @BotFather in Telegram.
+export TELEGRAM_BOT_TOKEN="PASTE_HERE"
+
+# Set your OWNER ID here in "".
+# To get your account ID, you can use any bot that has the /info command, e.g. @MissRose_bot
+export TELEGRAM_OWNER_ID="PASTE_HERE"
+
+# Set your bot log ID chat/channel here in "".
+# If you don't set it, the bot will send logs to the owner's PM.
+# Note that this does not require to run bot.
+# However, if you want to use this, remember to delete the hastag before the command below.
+# export LOG_CHAT_ID="PASTE_HERE"
+
+# Set your TENOR API here so that gifs appear with the commands /kill, /punch, /bite, /hug, /slap, /attack, /fed
+# Go to https://developers.google.com/tenor/guides/quickstart?hl=en and generate your tenor api key.
+# Note that this does not require to run bot.
+# However, if you want to use this, remember to delete the hastag before the command below.
+# export TENOR_API_KEY="PASTE_HERE"
+
+echo "done"
+
+# Use this command to start bot:
+# cd ~/zenthron && . ./env.sh && python zenthron.py
