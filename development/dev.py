2f1fc0bc82a93fbd71ecf3bc3460971c371ee8f6
R0X <patryk.bieganski1003@gmail.com>
2025-07-02 15:57:04 +0200

    Update dev.py

--- development/dev.py
+++ development/dev.py
@@ -3823,19 +3823,24 @@ async def handle_new_group_members(update: Update, context: ContextTypes.DEFAULT
     for member in update.message.new_chat_members:
         if member.id == context.bot.id:
             continue
-
+            
         base_text = ""
         if custom_text:
             base_text = custom_text
         elif member.id == OWNER_ID and OWNER_WELCOME_TEXTS:
+            owner_mention = member.mention_html()
             base_text = random.choice(OWNER_WELCOME_TEXTS)
         elif is_dev_user(member.id) and DEV_WELCOME_TEXTS:
+            user_mention = member.mention_html()
             base_text = random.choice(DEV_WELCOME_TEXTS)
         elif is_sudo_user(member.id) and SUDO_WELCOME_TEXTS:
+            user_mention = member.mention_html()
             base_text = random.choice(SUDO_WELCOME_TEXTS)
         elif is_support_user(member.id) and SUPPORT_WELCOME_TEXTS:
+            user_mention = member.mention_html()
             base_text = random.choice(SUPPORT_WELCOME_TEXTS)
         elif GENERIC_WELCOME_TEXTS:
+            user_mention = member.mention_html()
             base_text = random.choice(GENERIC_WELCOME_TEXTS)
         
         if base_text:
