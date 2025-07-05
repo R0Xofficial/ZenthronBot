c4c959302cd75b7fb42ebb6c9504ef62ca60b2f2
R0X <patryk.bieganski1003@gmail.com>
2025-07-05 10:06:58 +0200

    Update zenthron_test.py

--- zenthron_test.py
+++ zenthron_test.py
@@ -5868,7 +5868,7 @@ async def main() -> None:
         application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_note_trigger), group=0)
         
         application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, handle_new_group_members))
-        application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, check_gban_on_entry))
+        application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, check_gban_on_entry), group=-1)
         application.add_handler(MessageHandler(filters.StatusUpdate.LEFT_CHAT_MEMBER, handle_left_group_member))
 
         if application.job_queue:
