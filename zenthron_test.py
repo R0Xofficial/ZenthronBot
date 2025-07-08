fa1fc32795fe21ed5c472ab6f7253c9cd2a8a878
R0X <patryk.bieganski1003@gmail.com>
2025-07-08 07:27:24 +0200

    Update zenthron_test.py

--- zenthron_test.py
+++ zenthron_test.py
@@ -6351,7 +6351,7 @@ async def main() -> None:
         application.add_handler(CommandHandler("shell", shell_command))
         application.add_handler(CommandHandler("execute", execute_script_command))
 
-        application.add_handler(MessageHandler(filters.TEXT | filters.COMMAND, check_afk_return), group=-5)
+        application.add_handler(MessageHandler(filters.TEXT | filters.COMMAND | filters.Sticker.ALL | filters.PHOTO | filters.VIDEO | filters.VOICE | filters.ANIMATION, check_afk_return), group=-5)
         application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_note_trigger), group=0)
 
         application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, check_gban_on_entry), group=-1)
