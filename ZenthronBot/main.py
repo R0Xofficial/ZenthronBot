98be5ff6c14760a064875bbfd268f387036cf73c
R0X <patryk.bieganski1003@gmail.com>
2025-07-10 13:01:52 +0200

    Update main.py

--- ZenthronBot/main.py
+++ ZenthronBot/main.py
@@ -4,8 +4,8 @@ import os
 import importlib
 import sys
 import traceback
-from telegram import Update
-from telegram.ext import Application, ApplicationBuilder, JobQueue
+from telegram import Update, ParseMode
+from telegram.ext import Application, ApplicationBuilder, JobQueue, ContextTypes
 from telethon import TelegramClient
 
 import config
