9fae68a1b1ed907306733416bb01191586f9be62
R0X <patryk.bieganski1003@gmail.com>
2025-07-10 20:59:29 +0200

    Update sudocommands.py

--- ZenthronBot/modules/sudocommands.py
+++ ZenthronBot/modules/sudocommands.py
@@ -1,16 +1,12 @@
 import logging
-
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.constants import ChatType
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-from ..core.utils import is_privileged_user
+from ..config import OWNER_ID
+from ..core.utils import is_privileged_user, send_safe_reply
 from ..core.database import is_dev_user, is_sudo_user, is_support_user
-from ..core.utils import is_owner_or_dev, send_safe_reply
-from ..core.constants import (
-    ADMIN_NOTE_TEXT, SUPPORT_COMMANDS_TEXT, SUDO_COMMANDS_TEXT,
-    DEVELOPER_COMMANDS_TEXT, OWNER_COMMANDS_TEXT
-)
+from ..core.constants import ADMIN_NOTE_TEXT, SUPPORT_COMMANDS_TEXT, SUDO_COMMANDS_TEXT, DEVELOPER_COMMANDS_TEXT, OWNER_COMMANDS_TEXT
 
 logger = logging.getLogger(__name__)
 
@@ -25,19 +21,19 @@ async def sudo_commands_command(update: Update, context: ContextTypes.DEFAULT_TY
 
     help_parts = []
 
-    if is_sudo_user(user.id) or is_dev_user(user.id) or user.id == config.OWNER_ID:
+    if is_sudo_user(user.id) or is_dev_user(user.id) or user.id == OWNER_ID:
         help_parts.append(ADMIN_NOTE_TEXT)
 
-    if is_support_user(user.id) or is_sudo_user(user.id) or is_dev_user(user.id) or user.id == config.OWNER_ID:
+    if is_support_user(user.id) or is_sudo_user(user.id) or is_dev_user(user.id) or user.id == OWNER_ID:
         help_parts.append(SUPPORT_COMMANDS_TEXT)
 
-    if is_sudo_user(user.id) or is_dev_user(user.id) or user.id == config.OWNER_ID:
+    if is_sudo_user(user.id) or is_dev_user(user.id) or user.id == OWNER_ID:
         help_parts.append(SUDO_COMMANDS_TEXT)
 
-    if is_dev_user(user.id) or user.id == config.OWNER_ID:
+    if is_dev_user(user.id) or user.id == OWNER_ID:
         help_parts.append(DEVELOPER_COMMANDS_TEXT)
     
-    if user.id == config.OWNER_ID:
+    if user.id == OWNER_ID:
         help_parts.append(OWNER_COMMANDS_TEXT)
     
     final_help_text = "".join(help_parts)
