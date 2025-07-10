57ae1a0f6510b0088064b0e34747f11ca32c5b28
R0X <patryk.bieganski1003@gmail.com>
2025-07-10 13:32:13 +0200

    Update ai.py

--- ZenthronBot/modules/ai.py
+++ ZenthronBot/modules/ai.py
@@ -5,8 +5,7 @@ from telegram.error import BadRequest
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 import config
-from modules.utils import is_privileged_user
-from modules.utils import is_owner_or_dev, markdown_to_html, get_gemini_response
+from ..core.utils import is_owner_or_dev, markdown_to_html, get_gemini_response, is_privileged_user
 
 logger = logging.getLogger(__name__)
 
