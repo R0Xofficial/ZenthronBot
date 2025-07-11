586338f9b3e0a3141539b76f5cc1ac7d72cb6edf
R0X <patryk.bieganski1003@gmail.com>
2025-07-11 11:09:11 +0200

    Update main.py

--- ZenthronBot/main.py
+++ ZenthronBot/main.py
@@ -89,8 +89,7 @@ async def main() -> None:
         
         await application.initialize()
         await application.start()
-        allowed_updates = UpdateType.MESSAGE, UpdateType.CALLBACK_QUERY, UpdateType.CHAT_MEMBER, UpdateType.MY_CHAT_MEMBER, UpdateType.EDITED_MESSAGE
-        await application.updater.start_polling(allowed_updates=allowed_updates)
+        await application.updater.start_polling(allowed_updates=Update.ALL_TYPES)
         await telethon_client.run_until_disconnected()
 
         await application.updater.stop()
