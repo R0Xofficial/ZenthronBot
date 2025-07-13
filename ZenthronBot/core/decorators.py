09c0dfe336cb67d4442c0bb47a43b43b9531899b
R0X <patryk.bieganski1003@gmail.com>
2025-07-13 11:36:44 +0200

    Update decorators.py

--- ZenthronBot/core/decorators.py
+++ ZenthronBot/core/decorators.py
@@ -2,8 +2,9 @@ from functools import wraps
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from . import database
-from .. import config
+from .database import is_command_disabled_in_chat, is_module_disabled
+from ..config import OWNER_ID
+from .utils import _can_user_perform_action
 
 def check_module_enabled(module_name: str):
     def decorator(func):
@@ -11,12 +12,40 @@ def check_module_enabled(module_name: str):
         async def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE, *args, **kwargs):
             user = update.effective_user
             
-            if user and user.id == config.OWNER_ID:
+            if user and user.id == OWNER_ID:
                 return await func(update, context, *args, **kwargs)
 
-            if database.is_module_disabled(module_name):
+            if is_module_disabled(module_name):
                 await update.message.reply_text("This command is currently globally disabled.")
                 return
+
+            return await func(update, context, *args, **kwargs)
+        return wrapper
+    return decorator
+
+def command_control(command_name: str):
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE, *args, **kwargs):
+            chat = update.effective_chat
+
+            if not chat or chat.type not in ["group", "supergroup"]:
+                return await func(update, context, *args, **kwargs)
+
+            if is_command_disabled_in_chat(chat.id, command_name):
+                
+                is_admin = await _can_user_perform_action(
+                    update, 
+                    context, 
+                    'can_manage_chat', 
+                    failure_message="",
+                    allow_bot_privileged_override=True
+                )
+                
+                if is_admin:
+                    pass
+                else:
+                    return
 
             return await func(update, context, *args, **kwargs)
         return wrapper
