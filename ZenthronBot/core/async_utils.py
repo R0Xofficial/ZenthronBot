a2447e1bba628c841a6d32343d46006376670e09
R0X <patryk.bieganski1003@gmail.com>
2025-07-14 15:06:23 +0200

    Update async_utils.py

--- ZenthronBot/core/async_utils.py
+++ ZenthronBot/core/async_utils.py
@@ -1,7 +1,7 @@
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
 from typing import TypeVar, ParamSpec, Callable, Awaitable
-from functools import partial
+from functools import wraps
 
 P = ParamSpec("P")
 T = TypeVar("T")
@@ -9,15 +9,16 @@ T = TypeVar("T")
 _executor = ThreadPoolExecutor()
 
 def aioify(func: Callable[P, T]) -> Callable[P, Awaitable[T]]:
-    @partial(asyncio.coroutine)
-    def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
+    @wraps(func)
+    async def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
         if asyncio.iscoroutinefunction(func):
             raise TypeError("Cannot aioify a coroutine function.")
         
         loop = asyncio.get_running_loop()
         
         return await loop.run_in_executor(
-            _executor, partial(func, *args, **kwargs)
+            _executor, 
+            lambda: func(*args, **kwargs)
         )
 
     return wrapper
