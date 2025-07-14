00897a6e692d173036d94a68393704cdd8f68c49
R0X <patryk.bieganski1003@gmail.com>
2025-07-14 21:31:51 +0200

    Update ai.py

--- ZenthronBot/modules/ai.py
+++ ZenthronBot/modules/ai.py
@@ -3,6 +3,7 @@ from telegram import Update
 from telegram.constants import ParseMode
 from telegram.error import BadRequest
 from telegram.ext import Application, CommandHandler, ContextTypes
+from ..core.prefix_filter import PrefixFilter
 
 from ..config import GEMINI_API_KEY, OWNER_ID, PUBLIC_AI_ENABLED
 from ..core.utils import is_privileged_user, is_owner_or_dev, markdown_to_html, get_gemini_response
@@ -99,5 +100,7 @@ async def ask_ai_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 
 # --- HANDLER LOADER ---
 def load_handlers(application: Application):
-    application.add_handler(CommandHandler("setai", set_ai_command))
-    application.add_handler(CommandHandler("askai", ask_ai_command))
+    prefixes = ['/', '!']
+    prefix_filter = PrefixFilter(prefixes)
+    application.add_handler(CommandHandler("setai", set_ai_command, filters=prefix_filter))
+    application.add_handler(CommandHandler("askai", ask_ai_command, filters=prefix_filter))
