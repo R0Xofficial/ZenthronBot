9d66fbc9758d7e859e9cdbdb5dd9dbafcf26bf56
R0X <patryk.bieganski1003@gmail.com>
2025-07-14 17:17:32 +0200

    Create custom_handlers.py

---
+++ ZenthronBot/core/custom_handlers.py
@@ -0,0 +1,28 @@
+from typing import cast
+from telegram import Update
+from telegram.ext import CommandHandler, filters
+from telegram.ext.filters import Filter
+
+class CustomPrefixHandler(CommandHandler):
+    def __init__(self, command: str | list[str], callback, custom_prefixes: str | list[str] = '/', **kwargs):
+        
+        class CustomPrefixFilter(Filter):
+            def filter(self, update: Update) -> bool:
+                if not isinstance(update, Update) or not update.effective_message or not update.effective_message.text:
+                    return False
+                
+                message_text = update.effective_message.text
+                prefixes = custom_prefixes if isinstance(custom_prefixes, list) else [custom_prefixes]
+                commands = command if isinstance(command, list) else [command]
+                
+                for p in prefixes:
+                    for c in commands:
+                        if message_text.startswith(f"{p}{c}"):
+                            parts = message_text.split()
+                            if parts[0] == f"{p}{c}" or parts[0].startswith(f"{p}{c}@"):
+                                return True
+                return False
+
+        custom_filter = CustomPrefixFilter() & filters.COMMAND
+        
+        super().__init__(command, callback, filters=custom_filter, **kwargs)
