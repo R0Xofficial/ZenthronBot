1889668b7c3959432db2b2e5379d7262dfb90ce5
R0X <patryk.bieganski1003@gmail.com>
2025-07-21 17:41:49 +0200

    Update blacklists.py

--- ZenthronBot/modules/blacklists.py
+++ ZenthronBot/modules/blacklists.py
@@ -185,8 +185,8 @@ async def check_blacklist_handler(update: Update, context: ContextTypes.DEFAULT_
     if not is_user_blacklisted(user.id):
         return
 
-    always_allowed_commands = ['/start', '/help', '/info', '/id', '/rules']
-    appeal_chat_allowed_commands = ['/notes', '/warns', '/warnings']
+    always_allowed_commands = ['/start', '/help', '/rules', '/notes', '/warns', '/warnings']
+    appeal_chat_allowed_commands = ['/info', '/id']
 
     is_in_appeal_chat = (chat.id == APPEAL_CHAT_ID)
 
