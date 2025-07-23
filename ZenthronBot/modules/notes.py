c944c58da87ba1484a9de391ff1217017f6d2a50
R0X <patryk.bieganski1003@gmail.com>
2025-07-23 23:36:50 +0200

    Update notes.py

--- ZenthronBot/modules/notes.py
+++ ZenthronBot/modules/notes.py
@@ -28,9 +28,10 @@ async def save_note_command(update: Update, context: ContextTypes.DEFAULT_TYPE) 
         
     note_name = ""
     content = ""
-    replied_message = message.reply_to_message
 
-    if replied_message and not message.forum_topic_created:
+    if message.reply_to_message and not message.reply_to_message.forum_topic_created:
+        replied_message = message.reply_to_message
+        
         if not context.args:
             await message.reply_text("You need to provide a name for the note.\nUsage: /addnote <notename> (replying to a message)")
             return
